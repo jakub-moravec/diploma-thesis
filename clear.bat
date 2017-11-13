e9e4afd3b35d0843fd84de95dc793e4f0f66d330
Jakub Moravec <jakub.moravec@profinit.eu>
2017-11-13 15:34:51 +0100

    úvod init version

---
+++ clear.bat
@@ -0,0 +1,19 @@
+REM %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+REM % Clears working directory %
+REM %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+del /s /q .\dp-moravja8.aux
+del /s /q .\dp-moravja8.bbl
+del /s /q .\dp-moravja8.blg
+del /s /q .\dp-moravja8.log
+del /s /q .\dp-moravja8.lof
+del /s /q .\dp-moravja8.lot
+del /s /q .\dp-moravja8.nlo
+del /s /q .\dp-moravja8.out
+del /s /q .\dp-moravja8.pdf
+del /s /q .\dp-moravja8.synctex.gz
+del /s /q .\dp-moravja8.toc
+del /s /q .\uvod.aux
+del /s /q .\uvod.log
+del /s /q .\teorie.aux
+del /s /q .\teorie.log
